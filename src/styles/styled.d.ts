b044296ab4e742cce4b0caf39bdb1799ea7f3f8a
breno-aredes <breno_mg10@hotmail.com>
2025-04-26 12:32:59 -0300

    feat: add theme configuration and theme context provider

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,7 @@
+import "styled-components";
+import Theme from "./theme";
+type ThemeType = typeof Theme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+const Theme = {
+  colors: {
+    background: "#dddddd",
+    primary: "#7695EC",
+    secondary: "#ffffff",
+    text: "#000000",
+    border: "#777777",
+    placeholder: "#CCCCCC",
+    red: {
+      primaryRed: "#FF5151",
+    },
+    green: {
+      primaryGreen: "#4CAF50",
+    },
+  },
+} as const;
+
+export default Theme;
