166daaa1c2153500ea4daf4bf284b072195ded0d
breno-aredes <breno_mg10@hotmail.com>
2025-04-27 13:04:31 -0300

    feat: add save username

--- src/components/modal/loginModal/index.tsx
+++ src/components/modal/loginModal/index.tsx
@@ -1,22 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "..";
 import { ModalContent } from "./styles";
 import Button from "../../button";
 import { ButtonContent, InputContent } from "../../../styles/globalStyles";
+import { useUser } from "../../../hooks/useUser"; // caminho de exemplo
 
 const LoginModal = () => {
+  const { setUsername, username } = useUser();
   const [isVisible, setIsVisible] = useState(true);
+  const [inputUsername, setInputUsername] = useState("");
+
+  useEffect(() => {
+    if (username) {
+      setIsVisible(false);
+    }
+  }, [username]);
+
+  const handleLogin = () => {
+    if (inputUsername.trim() !== "") {
+      setUsername(inputUsername);
+      setIsVisible(false);
+    }
+  };
+
   return (
     <Modal isVisible={isVisible} setIsVisible={setIsVisible} isWide={false}>
       <ModalContent>
         <h1>Welcome to CodeLeap network!</h1>
+
         <InputContent>
           <label>Please enter your username</label>
-          <input placeholder="Your name"></input>
+          <input
+            placeholder="Your name"
+            value={inputUsername}
+            onChange={(e) => setInputUsername(e.target.value)}
+          />
         </InputContent>
 
         <ButtonContent>
-          <Button color="blue" onClick={() => setIsVisible(false)}>
+          <Button
+            color="blue"
+            onClick={handleLogin}
+            disabled={!inputUsername.trim()}
+          >
             Enter
           </Button>
         </ButtonContent>
