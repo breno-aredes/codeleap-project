dd9d0bc42714e7b91d12681f07b3fcb75c677c47
breno-aredes <breno_mg10@hotmail.com>
2025-05-01 12:58:16 -0300

    feat: add responsiveness to ModalContainer

--- src/components/modal/styles.ts
+++ src/components/modal/styles.ts
@@ -27,4 +27,7 @@ export const ModalContainer = styled.div<{ isWide: boolean }>`
   border: 1px solid ${({ theme }) => theme.colors.tertiary};
   opacity: 1;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  @media (max-width: 700px) {
+    width: 80vw;
+  }
 `;
